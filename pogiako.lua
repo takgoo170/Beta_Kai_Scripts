12911464e52524879a4a7a6478d20f40f468d829
takgoo170 <eddiejrsapian@gmail.com>
2025-06-07 13:09:09 +0800

    Update pogiako.lua

--- pogiako.lua
+++ pogiako.lua
@@ -130,7 +130,7 @@ Updates:AddParagraph({
 	Content = "HEADS UP! Changelogs will be seen in our discord community!"
 	})	
 ------------------- SERVER TAB ---------------
-
+Server:AddSection("Game Status")
 local MarketplaceService = game:GetService("MarketplaceService")
 
 -- Assume Server is your Fluent UI server object managing the UI
@@ -151,10 +151,29 @@ end
 local gameName = getCurrentGameName()
 
 Server:AddParagraph({
-    Title = "Current Game",
+    Title = "🎮 Current Game",
     Content = gameName ~= "" and gameName or "The game cannot be identified."
 })
 
+Timmessss = Server:AddParagraph({
+    Title = "🎮 Game Time",
+    Content = ""
+})
+function UpdateTime()
+    local GameTime = math.floor(workspace.DistributedGameTime + 0.5)
+    local Hour = math.floor(GameTime / (60^2)) % 24
+    local Minute = math.floor(GameTime / (60^1)) % 60
+    local Second = math.floor(GameTime / (60^0)) % 60
+    Timmessss:SetDesc(Hour.." Hour (h) "..Minute.." Minute (m) "..Second.." Second (s) ")
+end
+spawn(function()
+    while true do
+        UpdateTime()
+        wait(1)
+    end
+end)
+
+Server:AddSection("Server")
 Server:AddParagraph({
     Title = "Server Job ID",
     Content = game.JobId ~= "" and game.JobId or "Job ID not available."
@@ -201,6 +220,86 @@ Server:AddButton({
         end
     end
 })
+
+local lastTeleportTime = 0
+local teleportCooldown = 3
+Server:AddButton({
+    Title = "Rejoin Server",
+    Callback = function()
+        if tick() - lastTeleportTime >= teleportCooldown then
+            lastTeleportTime = tick()
+            game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)        
+        end
+    end
+})
+Server:AddButton({
+	  Title = "Server Hop",
+	  Callback = function()
+          Hop()
+      end
+})
+function Hop()
+    local PlaceID = game.PlaceId
+    local AllIDs = {}
+    local foundAnything = ""
+    local actualHour = os.date("!*t").hour
+    local Deleted = false
+    function TPReturner()
+        local Site;
+        if foundAnything == "" then
+            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+        else
+            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+        end        
+        local ID = ""
+        if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+            foundAnything = Site.nextPageCursor
+        end        
+        local num = 0
+        for i,v in pairs(Site.data) do
+            local Possible = true
+            ID = tostring(v.id)            
+            if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                for _,Existing in pairs(AllIDs) do
+                    if num ~= 0 then
+                        if ID == tostring(Existing) then
+                            Possible = false
+                        end
+                    else
+                        if tonumber(actualHour) ~= tonumber(Existing) then
+                            local delFile = pcall(function()
+                                AllIDs = {}
+                                table.insert(AllIDs, actualHour)
+                            end)
+                        end
+                    end
+                    num = num + 1
+                end
+                if Possible == true then
+                    table.insert(AllIDs, ID)
+                    wait(0.1)
+                    pcall(function()
+                        game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                    end)
+                    wait(1)
+                    break
+                end
+            end
+        end
+    end
+    function Teleport() 
+        while true do
+            pcall(function()
+                TPReturner()
+                if foundAnything ~= "" then
+                    TPReturner()
+                end
+            end)
+            wait(2)
+        end
+    end
+    Teleport()
+end
 ------------------ BLOX FRUITS TAB ------------------
 
 Bloxfruits:AddParagraph({
